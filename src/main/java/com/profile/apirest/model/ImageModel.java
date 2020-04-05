25235b2cd9433fd99b0800e821a694120ff1803d
Andreson Souza <andresonjazz@gmail.com>
2020-04-05 17:43:27 -0300

    Gere os get e seters

--- src/main/java/com/profile/apirest/model/ImageModel.java
+++ src/main/java/com/profile/apirest/model/ImageModel.java
@@ -40,4 +40,30 @@ public class ImageModel {
 		this.type = type;
 		this.pic = pic;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public byte[] getPic() {
+		return pic;
+	}
+
+	public void setPic(byte[] pic) {
+		this.pic = pic;
+	}
+	
+	
 }
