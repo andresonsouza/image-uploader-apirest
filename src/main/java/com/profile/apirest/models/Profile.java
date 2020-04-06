baf92871123d610c5ed898f63f1da059eaa039c3
Andreson Souza <andresonjazz@gmail.com>
2020-04-06 13:51:20 -0300

    Adicione o método post

--- src/main/java/com/profile/apirest/models/Profile.java
+++ src/main/java/com/profile/apirest/models/Profile.java
@@ -2,26 +2,41 @@ package com.profile.apirest.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "TB_PROFILE")
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@ToString
 public class Profile implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id")
+	@GeneratedValue
 	private Long id;
 
+	@Column(name = "name")
 	private String name;
 
+	@Column(name = "type")
 	private String type;
 
+	@Lob
+	@Column(name = "pic")
 	private byte[] pic;
 
 	public Long getId() {
@@ -56,5 +71,10 @@ public class Profile implements Serializable {
 		this.pic = pic;
 	}
 
-	
+	public Profile(String name, String type, byte[] pic) {
+        this.name = name;
+        this.type = type;
+        this.pic = pic;
+    }
+
 }
--- src/main/java/com/profile/apirest/resources/ProfileResource.java
+++ src/main/java/com/profile/apirest/resources/ProfileResource.java
@@ -1,17 +1,23 @@
 package com.profile.apirest.resources;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.profile.apirest.models.Profile;
 import com.profile.apirest.repository.ProfileRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class ProfileResource {
 
@@ -26,5 +32,18 @@ public class ProfileResource {
 	@GetMapping("/profile/{id}")
 	public Profile listaProfileId(@PathVariable(value="id") long id) {
 		return profileRepository.findById(id);
+		
+	}
+	
+	@PostMapping("/profile")
+		public Profile uploadImage(@RequestParam("myFile") MultipartFile file) throws IOException {
+		
+		Profile img = new Profile( file.getOriginalFilename(),file.getContentType(),file.getBytes() );
+		
+		final Profile savedImage = profileRepository.save(img);
+		
+        System.out.println("Image saved");
+
+        return savedImage;
 	}
 }
